663077b24a4902e1e27ebf68b40f2b1825da3953
Abdelaziz Mohamed Abdelaziz <abdelazizmohameddewedar@gmail.com>
2024-09-11 01:29:51 +0300

    Add PopupMenu to Chart Screen

--- lib/view/chart_screen.dart
+++ lib/view/chart_screen.dart
@@ -6,17 +6,42 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart' as fl;
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ChartScreen extends StatelessWidget {
+class ChartScreen extends StatefulWidget {
   const ChartScreen({super.key});
 
   @override
+  State<ChartScreen> createState() => _ChartScreenState();
+}
+
+class _ChartScreenState extends State<ChartScreen> {
+  @override
+  bool showName = false;
   Widget build(BuildContext context) {
     double totalBalance = BlocProvider.of<FetchDataCubit>(context).total;
-    
+
     return Scaffold(
       body: NestedScrollView(
           headerSliverBuilder: (_, __) => [
-                SliverAppBar(actions: [],
+                SliverAppBar(
+                  actions: [
+                    PopupMenuButton(onSelected: (value) {
+                      setState(() {
+                        showName = value;
+                      });
+                    },
+                        icon: const Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        itemBuilder: (context) {
+                          return [
+                            const PopupMenuItem(value:true ,child: Text('Show name'),),
+                            const PopupMenuItem(value:false ,child: Text('Show value'),)
+                          ];
+                        }),
+                       const SizedBox(width: 10,)
+                  ],
                   expandedHeight: 320,
                   floating: true,
                   pinned: true,
@@ -24,7 +49,7 @@ class ChartScreen extends StatelessWidget {
                   snap: true,
                   flexibleSpace: FlexibleSpaceBar(
                     background: Container(
-                    color: Colors.blue,
+                      color: Colors.blue,
                       height: MediaQuery.of(context).size.height * 0.4,
                       width: double.infinity,
                       child: Padding(
@@ -38,10 +63,13 @@ class ChartScreen extends StatelessWidget {
                               value: BlocProvider.of<FetchDataCubit>(context)
                                       .fetchCategorySum(category.name) /
                                   totalBalance,
-                              title:
-                                  ((BlocProvider.of<FetchDataCubit>(context).fetchCategorySum(category.name) / totalBalance) * 100).toStringAsFixed(2),
-                              titleStyle:
-                                  const TextStyle(fontSize: 20, color: Colors.black),
+                              title:showName ? category.name.toString() : ((BlocProvider.of<FetchDataCubit>(context)
+                                              .fetchCategorySum(category.name) /
+                                          totalBalance) *
+                                      100)
+                                  .toStringAsFixed(2),
+                              titleStyle: const TextStyle(
+                                  fontSize: 20, color: Colors.black),
                               radius: 100,
                             );
                           }).toList()),
